2f9e5d135e128c4849c578305861d62ab9d67315
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-12-10 15:31:30 +0800

    update list_all_devs

--- list_all_devs.js
+++ list_all_devs.js
@@ -1,4 +1,29 @@
 const pcap = require('pcap');
 
-console.log(pcap.findalldevs());
+/**
+ *
+ [ { name: 'eno1',
+    addresses: [ <1 empty item>, [Object], [Object], [Object] ] },
+ { name: 'any',
+    description: 'Pseudo-device that captures on all interfaces',
+    addresses: [] },
+ { name: 'lo',
+    addresses: [ <1 empty item>, [Object], [Object] ],
+    flags: 'PCAP_IF_LOOPBACK' },
+ { name: 'nflog',
+    description: 'Linux netfilter log (NFLOG) interface',
+    addresses: [] },
+ { name: 'nfqueue',
+    description: 'Linux netfilter queue (NFQUEUE) interface',
+    addresses: [] },
+ { name: 'usbmon1',
+    description: 'USB bus number 1',
+    addresses: [] },
+ { name: 'usbmon2',
+    description: 'USB bus number 2',
+    addresses: [] } ]
+ */
+const devs = pcap.findalldevs();
+
+console.log(JSON.stringify(devs));
 
