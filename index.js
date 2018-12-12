2438a5a9ea351a45dc731b21fc99a04773962b1b
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-12-12 15:29:27 +0800

    update list_all_devs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const pcap = require('pcap'),
     tcpTracker = new pcap.TCPTracker(),
-    pcap_session = pcap.createSession('eno1', "ip proto \\tcp");
+    pcap_session = pcap.createSession('eno1', "ether proto \\ip");
 
 tcpTracker.on('session', function (session) {
     console.log("Start of session between " + session.src_name + " and " + session.dst_name);
@@ -9,11 +9,24 @@ tcpTracker.on('session', function (session) {
     });
 });
 
-console.log(pcap.findalldevs());
+// console.log(pcap.findalldevs());
+
+function PcapHeader(raw_header) {
+    this.tv_sec = raw_header.readUInt32LE(0, true);
+    this.tv_usec = raw_header.readUInt32LE(4, true);
+    this.caplen = raw_header.readUInt32LE(8, true);
+    this.len = raw_header.readUInt32LE(12, true);
+}
 
 pcap_session.on('packet', function (raw_packet) {
-    console.log(raw_packet);
+    // console.log(raw_packet);
     const packet = pcap.decode.packet(raw_packet);
-    console.log(JSON.stringify(packet));
-    tcpTracker.track_packet(packet);
+    let pcap_header = new PcapHeader(raw_packet.header);
+    console.log('----');
+    console.log(pcap_header.len);
+    console.log(raw_packet.buf.length);
+    console.log(packet.payload.payload.payload.data.length);
+    console.log('----');
+    // console.log(JSON.stringify(packet));
+    // tcpTracker.track_packet(packet);
 });
